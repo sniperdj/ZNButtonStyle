f69166622b4fe18488f8371158b706935e121791
郑楠 <sniper_zheng@163.com>
2017-12-10 14:46:45 +0800

    create a custom btn style of category

---
+++ ZNButtonStyleDemo/ZNButtonStyleDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZNButtonStyleDemo
+//
+//  Created by Sniper on 10/12/2017.
+//  Copyright © 2017 Sniper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
